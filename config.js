2a6a98c00bc5ac9fabfe4a6117fbb227109546eb
quangminh0306 <90037694+quangminh0306@users.noreply.github.com>
2021-09-03 17:34:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Gửi cho ... <3",
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'hiiii luuuuu!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'em thích anh à 🥰??',
+    desc: 'đừng nói dối t đấm chết mẹ mày',
+    btnYes: 'đr,em thích anh <33',
+    btnNo: 'm bị ảo à',
+    question: 'sao em lại thích anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'vì anh đẹp trai s1tg <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'nên sendnude đi em=))))).',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
