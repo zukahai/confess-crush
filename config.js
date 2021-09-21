5f70dcbcdda8fb9ff8b80c15c172ed22f654d7bc
EiraLe <91141847+EiraLe@users.noreply.github.com>
2021-09-21 23:08:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    titleWeb: "From ToNgan with love",
+    introTitle: 'Hi Nguyên Ngáo Ngơ',
+    introDesc: `Trái đất vốn lạ thường. 
+    Mà sao Nguyên cứ đi nhầm đường. 
+    Lạc vào tim ai lẻ loi. 
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnIntro: '^^Hí Hí^^',
+    title: 'Phải chăng Nguyên đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng Nguyên đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'U là trời. Tui mê như điếu đổ, mê lắm lắm',
+    btnNo: 'Mơ đi nhaaa. Tui mê A7 rùi, còn có cả Sugar Baby nữa nhé. lêu lêu :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại mê mình thế nhờ <3',
+    btnReply: 'Tại vì duyên số nên chúng mình... Cơ mà Ngân dễ thương <3 <3',
+    reply: 'Mê thì mê mà không mê thì mê <33333333',
+    mess: 'Tui biết mà 🥰. Cơ mà ... Tui bị mê gái xinhhh rùi. Hẹn ông kiếp sau, kiếp này ta hông thấy nhau 😘😘',
+    messDesc: 'Ông về với a7 điiiii.',
+    btnAccept: 'Thôi mà <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //https://www.facebook.com/nganle0719/
 }
