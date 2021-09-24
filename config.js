78a2e3e4e34e1b9e610b1d446856380d74b5e8d3
Greentea2306 <91297227+Greentea2306@users.noreply.github.com>
2021-09-24 08:36:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
+    titleWeb: "Gửi bạn MT iu dấu",
     introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    introDesc: `Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.`,
+    btnIntro: 'Hứa trả lời THẬT luôn',
+    title: 'Cậu vẫn yêu tớ có phải không nào ._.',
+    desc: 'Nếu cậu ko trả lời mà thoát ra tức là muốn làm ng iu tớ đó nha :v ',
+    btnYes: 'Vâng! Yêu nhiều lắm luôn <33',
+    btnNo: 'Không nha!kí cái đầu bây giờ :3',
+    question: 'Vậy sao hổm giờ không chịu nói ra?',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại ngại á vì mà mình nhớ cậu nhiều lắm',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Còn chờ gì mà ko inbox cho tớ đi nàoooooo.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
