a7f115ef2647337645eac36bf1442f4f97fa8750
YêuShi <87214367+122005h@users.noreply.github.com>
2021-09-24 11:39:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Trần Thị Hồng Vân😘',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Shi cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnYes: 'Chị cũng thích e lâu rồi nhưng chị ngại ý😳',
+    btnNo: 'E mơ à😒',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Shi lại yêu em 🤭',
+    btnReply: 'Gửi cho Em nhé😊',
+    reply: 'Yêu thì yêu mà không yêu thì yêu 🥰',
+    mess: 'E biết mà🤗.E cũng yêu Shi nhiều lắm luôn ý :333',
+    messDesc: 'Tối nay 8h, vô sp cho e nhé🥰.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
