a0c2610d846d5dc7f3e7d29fb2c1441300cea2d2
Taiwalker12 <91471675+Taiwalker12@users.noreply.github.com>
2021-09-27 21:56:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Tên người yêu bạn Nhím ú nu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
