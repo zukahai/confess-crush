1b1c63629d1f8d2d2764bf8dfe10e98030d12320
tien09101017 <86121914+tien09101017@users.noreply.github.com>
2021-10-02 19:57:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi đến Han <3",
+    introTitle: 'Han Han',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
