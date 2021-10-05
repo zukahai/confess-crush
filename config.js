013c508876a11d4eb25c03b185eae678eac17857
hinmei <91963191+hinmei@users.noreply.github.com>
2021-10-05 21:09:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "heloo Yêu",
+    introTitle: 'Nọc Chút',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao chị cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: '^^nhấn vào đây nè bà chị già^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi cho người yêu của chị <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Mình biết mà 🥰. Bạn yêu mình nhiều hehe biết ngay mà mình cũng zậy 😘😘',
+    messDesc: 'Ngày nay làm việc có mệt ko ? Đương nhiên là có rồi nhưng mình cũng chỉ biết vậy thôi chứ có làm được gì đâu. Chỉ muốn nói yêu bạn nhiều nhiều ớiiii là nhieều. Gửi triệu tymmmmm từ ĐakLak tới Sài Gòn nơi chị :3 .',
+    btnAccept: 'Yêu Mei :3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
