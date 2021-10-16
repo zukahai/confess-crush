18dd709807b365795553c95f244d1a58a31adef6
luanhoang05 <92615366+luanhoang05@users.noreply.github.com>
2021-10-16 10:14:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
+    titleWeb: "LUÂN HOÀNG",
     introTitle: 'CLB SVHD PHU Cu',
     introDesc: `Suốt ngày chỉ giao nhiều dl cho Nuaan thôiii`,
     btnIntro: '^^Ấn nhanh^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'Lần sau không giao cho Nuaan dl nx nhé 😝',
+    desc: 'Chuyển cho Nuaan cả CLB cũng được ý ',
+    btnYes: 'Okela',
+    btnNo: 'Không nha ',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại chuyển cho mình đấy',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tôi thích chuyển cho bạn đấy',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tối nay 7h, chuyển giấy tờ nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/nuaan.123205/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
