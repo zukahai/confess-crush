5e4bc221305244ea9321ba49d7292d88e73c48c3
luanhoang05 <92615366+luanhoang05@users.noreply.github.com>
2021-10-16 09:53:53 +0700

    nuaan

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    introTitle: 'CLB SVHD PHU Cu',
+    introDesc: `Suốt ngày chỉ giao nhiều dl cho Nuaan thôiii`,
+    btnIntro: '^^Ấn^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
