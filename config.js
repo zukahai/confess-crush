00e4ed05dc0f1cde2d4c87f59543c63054ae2c0c
vuquanghuy22 <91678123+vuquanghuy22@users.noreply.github.com>
2021-10-28 09:44:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun <3', 
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
