afcc36c9a467231938630c3f5c728f0807068157
Sindyzz <93513347+Sindyzz@users.noreply.github.com>
2021-11-01 15:01:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Nguyễn Thị Kim Hằng',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
