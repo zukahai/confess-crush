2e963528d7bedd810da8b32c5506bc62e6299b32
tuyen061103 <93665094+tuyen061103@users.noreply.github.com>
2021-11-03 22:12:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Hello BâyyBêê",
-    introTitle: 'NGỌC HIỀN',
+    introTitle: 'Gửi đến người ngồi trước màn hình',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -9,12 +9,12 @@ const CONFIG = {
     title: 'Yêu hay không yêu không yêu hay yêu nói một lời 🥰',
     desc: 'Nói thật đê yêu hay không yêu? ',
     btnYes: 'Yêu lắm <33',
-    btnNo: 'Không nha :3',
+    btnNo: 'Đéo yêu :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Vì anh đẹp zai hiền lành vui tính <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì anh đẹp zai hiền lành vui tính <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Hehe yêu em nhiều lắm.',
+    messDesc: 'Hehe em thì sao?',
     btnAccept: 'Em cũng thế <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
