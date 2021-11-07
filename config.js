7e57679a77a6fd5b39a292d0c9d4b3af031102f7
Nguyenthat84 <93869173+Nguyenthat84@users.noreply.github.com>
2021-11-07 20:43:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Hello Chang <3",
+    introTitle: 'Đỗ Thị Thu Trang',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao mình cứ đi nhầm đường
+    Lạc vào tim Trang lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnIntro: '^^HiHi IU Chang^^',
+    title: 'Phải chăng mình đã yêu Trang ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng mình đã say Trang ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trang có iuuu mình không <3',
+    btnReply: 'Gửi <3',
+    reply: 'Không yêu Thật cũng phải yêu <3',
+    mess: 'Ha Ha 🥰. Yêu Trang nhiều nhiều 😘😘',
+    messDesc: 'Sáng mai gọi mình dậy học onl nha nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://facebook.com' //dthutrang1510/. VD: https://m.me/nam.nodemy
 }
