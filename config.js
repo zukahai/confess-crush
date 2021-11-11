0a0999cc1d6cf435d8c53d4a14d820bf4c66a4c2
Cutoia5 <94130063+Cutoia5@users.noreply.github.com>
2021-11-11 23:45:05 +0700

    Mai n
    Không có

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Hôm nào đi uống trà sữa nhé :>",
+    introTitle: 'Mai',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Đi uống trà sữa với Cường vui cực',
+    desc: 'Đi uống trà sữa với Cường vui cực',
+    btnYes: 'Ok nuôn nhé ',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Tại sao bạn lại chọn đi uống trà sữa với mình',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'mình thích thế :>>',
+    mess: 'Được',
+    messDesc: 'Mấy hôm mình qua đón đi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
