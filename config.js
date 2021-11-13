afe35f8eee9be5db02fd0039b2df40c32bf43cf3
Phoanh <93595836+Phoanh@users.noreply.github.com>
2021-11-13 18:47:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,22 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    titleWeb: "Gửi các bé iu của tui",
+    introTitle: 'Babi K63',
+    introDesc: `Đã apply SEED chưa???`,
+    btnIntro: 'Click zô đây',
+    title: 'Hỏi lần cuối, đã apply SEED chưa nào?',
+    desc: 'Apply mauuuuuu',
+    btnYes: 'Vâng ạaaa ❣️',
+    btnNo: 'Không 😏',
+    question: 'Bướng nhỉ? Giỏi thì cho nhau xin cái hẹn
+    
+    hẹn hò',
+    btnReply: 'Gửi lời nhắn của bạn',
+    reply: 'Chị là con nghiện nhưng là nghiện ma tuý
+    gặp được ems thì hứa là mai cai
+    vì ems như lấn át cả tâm trí
+    heroin my mind',
+    mess: '♥️',
+    messDesc: '',
+    btnAccept: 'Em cũng iu chịiii',
+    messLink: 'https://www.facebook.com/phoanhcharming264/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
