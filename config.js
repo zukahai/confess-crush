48d6b88c84188636b50d53b76e5e0419ba637105
laggin <93973372+vinhdayyyy@users.noreply.github.com>
2021-11-16 19:46:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ const CONFIG = {
     btnNo: ' honggg iu :)))',
     question: ' sao cậu thích mình dạaaa :33',
     btnReply: 'Ấn đây',
-    reply: ' tại vì cậu giống tớ mà cũng thân thiện',
+    reply: ' tại vì cậu nhiều điểm chung giống tớ mà cũng thân thiện',
     mess: ':333 cảm ơnn cậuuu nhìu nhaaaa',
     messDesc: ' Tớ cũng thích cậu nhiều lắm á. Tham gia discord được 2 tháng mà cũng biết cậu lâu rồi bây giờ mới dám nói *hí hí ngại* :33.Giờ thì out page giúp tớ cái.',
     btnAccept: 'okeee luôn cậuuu  :33',
-}
+}   messLink:'https://www.facebook.com/messages/t/100029952212373'
