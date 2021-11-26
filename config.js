7eb982ca03757507a406aa8c9a337681d741fb1b
Justcallmeleej <95065498+Justcallmeleej@users.noreply.github.com>
2021-11-26 11:00:36 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "bluemoon",
+    introTitle: 'You',
+    introDesc: `Hey you! Long time no see!
+    Thanks! You're awesome for being fond of me
+    It'll Be Great If We Are Friends
+    Friends...tks`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Just friends 🥰',
+    desc: 'Once, normal friends ',
+    btnYes: 'okelaa <33',
+    btnNo: 'yep,i like it :3',
+    question: 'we're more suited to being friends ❤',
+    btnReply: 'Gửi cho bạn ❤',
+    reply: 'Uh, i think so... ',
+    mess: 'T biết mà 🥰.hiuhiuuu 😘😘',
+    messDesc: 'goodbye and we are friends ',
+    btnAccept: 'Okiiiii lun ❤',
+    messLink: 'http://fb.com'
 }
