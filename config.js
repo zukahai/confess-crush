7af8f67bfaece5a7fd0d1a3918cb4451f0d096a6
tienphuoc1234 <95084926+tienphuoc1234@users.noreply.github.com>
2021-11-26 20:45:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "HÉ LÔ BAN ",
+    introTitle: 'TRINHJ CƯỜNG THỊNH',
+    introDesc: `TRẢ LỜI CÂU HỎI ĐI NHÁ
+    TỚ ĐẸP TRAI LẮM ĐÚNG KHONG <3,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại THẤY TỚ ĐẸP TRAI',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'TỚ THẤY BẠN ĐẸP TRAI LẮM LUÔN MÀ',
+    mess: 'Mình biết mà 🥰. CẢM ƠN BAN NHE 😘😘',
+    messDesc: 'MÌNH ĐẸP TRAI HƠN BAN ĐÚNG KHONG.',
+    btnAccept: 'DUNG RUI <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+}.
