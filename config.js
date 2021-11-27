bc835d2a238e6d31deaa3023d3dea8f00b47450f
Justcallmeleej <95065498+Justcallmeleej@users.noreply.github.com>
2021-11-27 11:14:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100047795043893' 
+    messLink: 'https://www.facebook.com/' 
 }
