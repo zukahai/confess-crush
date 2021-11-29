9ce99543c81e12e166a99c5dba633034f1663629
Kerezarina <95195506+Kerezarina@users.noreply.github.com>
2021-11-29 08:17:02 +0700

    Update config.js
    const CONFIG =https://github.com/zukahai/Confess-Crush {yeu
        titleWeb: "yeu",
        introTitle: 'satoru gojo',
        introDesc: `Trái đất vốn lạ thường
        Mà sao em cứ đi nhầm đường
        Lạc vào tim anh lẻ loi
        Đằng sau chữ yêu đây là thương`,
        btnIntro: '^^HiHi^^',
        title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
        desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
        btnYes: 'Thích lắm <33',
        btnNo: 'Không nha :3',
        question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
        btnReply: 'Gửi cho bạn <3',
        reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
        mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
        messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
        btnAccept: 'Okiiiii lun <3',
        messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
    }

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+const CONFIG =https://github.com/zukahai/Confess-Crush {yeu
+    titleWeb: "yeu",
+    introTitle: 'satoru gojo',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
