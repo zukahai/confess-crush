0a915b2a011c15561c0bdea8e1665fd793842262
trhgw05 <95230569+trhgw05@users.noreply.github.com>
2021-11-29 22:19:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Thế giới của meihun",
-    introTitle: 'Phạm Tiến Đạt',
+    introTitle: 'Chào anh!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Chào em',
+    btnIntro: 'Chào em!',
     title: 'Anh ơi',
-    desc: 'Nếu anh không phiền thì làm người yêu em nhá, nếu anh không phiền thì nhận tình yêu em nhá ',
-    btnYes: 'Anh đồng ý!',
-    btnNo: 'Không nha:( ',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại chọn em <3',
-    btnReply: 'Gửi cho em',
+    desc: 'Nếu anh không phiền thì làm người yêu em nhá, nếu anh không phiền thì nhận tình yêu em nháaa ',
+    btnYes: 'Anh đồng ý 🥰',
+    btnNo: 'Không đâu 🥺',
+    question: 'Thế giới có hơn 7 tỉ người mà sao anh lại chọn em :>',
+    btnReply: 'Gửi cho em ❤️',
     reply: 'Em đáng yêu, em cute, em dễ thương, em tuyệt vời nhất <33333333',
-    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'Vậy giờ chúng mình là của nhau rùi nha :33',
+    mess: 'Em biết mà 😂🤣. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Vậy giờ chúng mình là của nhau rùi nha 🥰',
     btnAccept: '<3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
