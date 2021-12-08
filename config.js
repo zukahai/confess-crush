76274d830cca31551f3f50cc90dddb5b83a3f91a
taovietduc193 <95725645+taovietduc193@users.noreply.github.com>
2021-12-08 17:19:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+	titleWeb : "Send to My crush!!!" ,
+	introTitle : 'Huỳnh Tuấn Duy' ,
+	introDesc : Thường lệ vốn trái đất
+	Mà sao em cứ đi nhầm đường
+	Lạc vào tim anh lẻ loi
+	Đằng sau chữ yêu đây là thương` ,
+	btnIntro : '^^ HiHi ^^' ,
+	title : 'Phải chăng em yêu ngay từ cái nhìn đầu tiên ,
+	desc : 'Phải chăng em đã nói ngay từ khi thấy nụ cười xuất hiện' ,
+	btn Có : 'Thích lắm <33' ,
+	btnNo : 'Không nha: 3' ,
+	question : 'Trên thế giới hơn 7 tỉ lệ người sao bạn lại yêu mình <3' ,
+	btnReply : 'Gửi cho bạn <3' ,
+	reply : 'Yêu thì yêu mà không yêu thì yêu <33333333' ,
+	mess : 'Tao biết mà Yêu mày nhiều ,
+	messDesc : 'Mốt cưới Tao nha!!! " ,
+	btn Chấp nhận : 'Okiiiii lun <3' ,
+	messLink : 'https://github.com/zukahai/Confess-Crush' // liên kết lộn xộn của các bạn. VD: https://m.me/nam.nodemy
+	}
