3a4f21b5cfbaf00045d062588411c777a415c250
Dinh Giang Long <75195080+GiangLong2520@users.noreply.github.com>
2021-12-10 18:42:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "gửi ngừi iu anh",
+    introTitle: 'châm châm',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
