2ebeca3df65b7f746464b18c48deac21f168f4a5
levanhuy12345 <92557392+levanhuy12345@users.noreply.github.com>
2021-12-14 10:37:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Làm người anh nha bé :3 ",
+    introTitle: 'Công CHÚA HAY DỖI CỦA ANHHH',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: '^^BẤM VÀO BABEEE^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnYes: 'YÊU CÔNG CHÚA HUY NHẤT <33',
+    btnNo: 'GHÉT MÀY NHẤT NHAA :3',
+    question: 'SAO CÔNG CHÚA HAY DỖI LẠI YÊU CÔNG CHÚA HUYYY DỌOO <3',
+    btnReply: 'Gửi cho CÔNG CHÚA HUY <3',
+    reply: 'VÌ CÔNG CHÚA HUY DỄ THƯƠNG DỊU DÀNG VÀ YÊU EMMM <33333333',
+    mess: 'ANHH biết mà 🥰. Yêu EMM nhiều nhiều 😘😘',
+    messDesc: 'CHÚC EM SINH NHẬT VUI VẺ VÀ CÓ ĐƯỢC ANH NGƯỜI YÊU NHAAA ',
+    btnAccept: 'DẠ VÂNG ẠAA <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
