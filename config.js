51d40479ab1f14de7562b79be2619f42955c59de
QUANIUNHUY <96876616+QUANIUNHUY@users.noreply.github.com>
2021-12-30 21:51:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Do anh Quân đtrai lại còn dthw với lại thương bé nữa <33',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Nhớ xem xog kiếm gì ăn đi nha.',
+    messDesc: 'Nhớ xem xog kiếm gì ăn đi nha.Rồi ib cho anh nha.Iu Bé nhà!',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100068385577403' 
 }
