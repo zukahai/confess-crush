c6a73f199f685864abb0c05e2897bc0806d256ff
Lmaolmao108 <97033715+Lmaolmao108@users.noreply.github.com>
2022-01-03 20:13:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Heo',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnIntro: '^^ngu^^',
+    title: 'Ngu quá',
+    desc: 'Phải chăng t sắp đánh m =))',
+    btnYes: 'm ngu',
+    btnNo: 'm khôn',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao m ngu thế hả',
+    btnReply: 'Gửi cho t địa chỉ',
+    reply: 'Ngu ngốc,
+    mess: 'Mình biết mà bạn thích oánh nhau',
+    messDesc: 'Tối nay oánh nhau ko.',
+    btnAccept: 'ok thằng quần què,
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
