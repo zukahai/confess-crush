1404345157e351a30fa561ddde21546b300cd6bb
mailinhyee <75652345+mailinhyee@users.noreply.github.com>
2022-01-14 10:56:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Ahihi",
+    introTitle: 'Nguyễn Văn A',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
