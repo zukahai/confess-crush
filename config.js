fceaed72de3bb624200fce0dc8c0825bce0fbf69
Quangvtv <101050988+Quangvtv@users.noreply.github.com>
2022-03-06 21:11:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "hi chao cau",
+    introTitle: 'vien cuteee',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
