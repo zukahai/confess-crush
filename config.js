8180fbfe115a7047d3436449218bc13e8e3328bb
hienakapro <102596810+hienakapro@users.noreply.github.com>
2022-03-29 19:57:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "tình iu của em",
+    introTitle: 'Toàn Thắng ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
