401d0b5fd22eff44daa48fac274cbe8e430c2366
hangvo18h3 <102867973+hangvo18h3@users.noreply.github.com>
2022-04-02 22:31:20 +0700

    Iuanh.com.vnn

--- config.js
+++ config.js
@@ -5,16 +5,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: ':3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu mình em <3',
+    btnReply: 'Gửi câu trả lời <3',
+    reply: 'vì em cư tê s1 thế giới',
+    mess: 'Em bíc mà 🥰. Yêu bạn nhìu nhìu ❤️',
+    messDesc: 'bae cụ e ngủ ngon nhó <3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
