e331ea0f7b3486d63b5dcb2d10f44e1cdcd21170
hangvo18h3 <102867973+hangvo18h3@users.noreply.github.com>
2022-04-02 22:24:41 +0700

    Iuanh.com.vn

--- config.js
+++ config.js
@@ -10,11 +10,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu mình em <3',
+    btnReply: 'Gửi câu trả lời <3',
+    reply: 'Vì em cư tê s1 thế giới',
+    mess: 'Em bíc mà 😳 Yêu anh nhìu nhìu ❤',
+    messDesc: 'Iu bae cụa em nhứt :3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+
