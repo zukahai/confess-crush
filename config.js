914d1319a9791343bad81ba5fa025c1e143917a8
phamvietnga <103841261+phamvietnga@users.noreply.github.com>
2022-04-16 22:51:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Ngày mai bạn bao mình trà sữa nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://github.com/bichvan/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
