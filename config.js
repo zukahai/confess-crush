8da06d06128ec4cb01038a25ccf1c025ff45a68e
cohaiquemua <105145803+cohaiquemua@users.noreply.github.com>
2022-05-08 15:04:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "tỏ tềnh =))",
+    introTitle: 'Trần QUốc Khánh * Khénh * ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnIntro: 'ulatroi sến nổi da gà :UU,
+    title: 'Phải chăng anh đã yêu ',
+    desc: 'Phải chăng anh đã say',
+    btnYes: 'yess thích lắm <33',
+    btnNo: 'Không nha >:0 ,
+    question: 'seo you lại yêu me <3',
+    btnReply: 'Gửi cho người yêu tương lai <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    mess: 'me biết mà 🥰. Yêu you nhiều nhiều 😘😘',
+    messDesc: 'gòi chối kèo nha you là của me ùi không được thất hứa =^^',
+    btnAccept: 'Okiiiii lun :33,
+    messLink:https://www.facebook.com/hongtuoi.pham.2020
 }
