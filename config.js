bcde1af4b9a10f1e73e0bb251ea133cb7999a67c
dat0999 <106955763+dat0999@users.noreply.github.com>
2022-06-06 15:21:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+        titleWeb: "Annhh laa cuaa emm",
+    introTitle: 'Gửi Văn Đức iu dấuu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
