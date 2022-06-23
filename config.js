b77482d3fc280504208b1af03afa64e47489d7ae
akiosmile <108071032+akiosmile@users.noreply.github.com>
2022-06-23 19:41:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "guiem",
+    introTitle: 'Phthuyyy',
+    introDesc: `Click vào đây hoặc là em đi chết đi,argggghhh`,
+    btnIntro: '^^<3 traitim^^',
+    title: ' hmmm, anh tên là Tuấn nhưng thay vì thế em có thể gọi là anh yeeuuuuuuu:3',
+    desc: 'em mà thoát ra thì em chết đi, arggghhhh ',
+    btnYes: 'dạ , được ạ <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    question: 'Thứ 2 là ngày đầu tuần,bé hứa cố gắng chăm ngoan,thứ 3 thứ 4 thứ 5 ngày nào cũng luôn cố gắng,thứ 6 rồi đến thứ 7 tuần này em có rảnh không? <3',
+    btnReply: 'gửi anh cùng trăm lời thương :3 <3',
+    reply: 'Dạ,em rảnh ạ,sẽ rất tuyệt nếu được ngồi sau xe anh đi đây đó :3 <33333333',
+    mess: 'hiiiiiii,bé inbox cho anh xin giờ giấc,anh qua đón bé nhaaa',
+    messDesc: 'vậy.... em ăn cơm chưa ?',
+    btnAccept: 'yeuanh <3',
+    messLink: 'https://www.facebook.com/messages/t/100006131343650/
 }
