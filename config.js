ef8860e656b3b4e06edaed2ce0e67ee5bf81cb51
YêuShi <87214367+122005h@users.noreply.github.com>
2022-06-26 10:31:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Như Linh😘',
+    introTitle: 'Như Mộc😘',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Linh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Mà sao Mộc cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Chị cũng thích e lâu rồi nhưng chị ngại ý😳',
-    btnNo: 'E mơ à😒',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao Linh lại yêu em 🤭',
-    btnReply: 'Gửi cho Em nhé😊',
+    title: 'Phải chăng a đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng a đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Em cũng thích a lâu rồi nhưng e ngại ý😳',
+    btnNo: 'A mơ à😒',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Mộc lại yêu anh 🤭',
+    btnReply: 'Gửi cho anh nhé😊',
     reply: 'Yêu thì yêu mà không yêu thì yêu 🥰',
-    mess: 'E biết mà🤗.E cũng yêu Linh nhiều lắm luôn ý :333',
-    messDesc: 'Tối nay 8h, vô sp cho e nhé🥰.',
+    mess: 'A biết mà🤗.A cũng yêu Mộc nhiều lắm luôn ý :333',
+    messDesc: 'Tối nay 8h, vô sp cho a nhé🥰.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/LoveLinh/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://github.com/LoveMoc/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
