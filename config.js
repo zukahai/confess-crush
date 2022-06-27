918a4a3d0b91fa7f9d1b041db51af4e5d10661fc
dieuhandsome <93961636+dieuhandsome@users.noreply.github.com>
2022-06-27 16:49:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "LOVE OR NOT",
+    introTitle: 'Dieuhandsome',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'Ly ơi Ly có thích tôi k  🥰',
+    desc: ' ',
+    btnYes: 'Em thích a Diệu lắm  <33',
+    btnNo: 'Không , mơ đi :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình a <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    reply: 'Vì a Diệu qúa  tuyệt vời  <33333333',
+    mess: 'Mình biết mà 🥰. Nhưng mà a chịu thiệt yêu em được  😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://m.me/anhchang.nobita.5' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
