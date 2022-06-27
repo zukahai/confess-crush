e18173598e2a7332cb13ac4751adcd7ca059dd73
dieuhandsome <93961636+dieuhandsome@users.noreply.github.com>
2022-06-27 17:20:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "LOVE OR NOT",
     introTitle: 'Dieuhandsome',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Anh không yêu em , anh yêu ai 
+    Anh không yêu ai , ai yêu em
+    Không ai yêu em , tôi yêu em :3
+    
     btnIntro: '^^HiHi^^',
     title: 'Ly ơi Ly có thích tôi k  🥰',
     desc: ' ',
     btnYes: 'Em thích a Diệu lắm  <33',
     btnNo: 'Không , mơ đi :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình a <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu mình anh <3',
+    btnReply: 'Gửi <3',
     reply: 'Vì a Diệu qúa  tuyệt vời  <33333333',
-    mess: 'Mình biết mà 🥰. Nhưng mà a chịu thiệt yêu em được  😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Mình biết mà 🥰. Nhưng không sao a chịu thiệt yêu em được  😘😘',
+    messDesc: 'Tối nay a qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/anhchang.nobita.5' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
