0dfb79e19c5257e6a0ad00c7e07d31618f6bb317
JackVictory <108384036+JackVictory@users.noreply.github.com>
2022-06-29 12:14:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    titleWeb: "Thanh Ngo",
+    introTitle: 'He Luu Cậu!!!',
+    introDesc: `Cậu Cứ Như Nút Like Ấy Nhỉ
+    Tớ Phải Giữa Mới Chạm Được Trái Tim ❤️`,
+    btnIntro: 'HiHii 😉',
+    title: 'Cậu Thích Tớ Có Phải Không Nào ._.',
+    desc: 'Nếu cậu ko trả lời mà thoát ra tức là muốn làm chó rồi đó nha :v ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Thích thì thích mà không thích thì thích <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Vậy còn chờ gì nữa mà ko inbox cho tớ đi nàooo.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/tinthanhngo.6112000/'
 }
