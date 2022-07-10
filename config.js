5a5f55eaf88ae6f5eee7f9cb9bc0ac240110adbf
homnaytuivui <109037102+homnaytuivui@users.noreply.github.com>
2022-07-11 00:38:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Huydepzai",
+    introTitle: 'Gửi bẹn cute gì đó',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnNo: 'Không nha=))',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình cơ <3',
+    btnReply: 'Gửi cho Huy <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3333333',
+    mess: 'Mình biết mà, bạn hok iu là mềnh đấm à. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Còn ko mau ib cho mềnh đuy, mãi iu à:V.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
