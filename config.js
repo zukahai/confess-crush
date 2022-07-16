84af35d02e57f320788a5f05e5914d41a12cf726
monquanho <109408810+monquanho@users.noreply.github.com>
2022-07-16 15:15:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Món quà cuối cùng của mèo",
+    introTitle: 'For the most beautiful girl',
+    introDesc: `mèo méo meo mèo meoooo
+    mai Đoan bước sang chương mới của mình rồi 
+    nên mèo sẽ tặng Đon 1 ngôi sao có chứa điều ước của mèo^^ 🌠 `,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'mèo ước cho Đon gặp nhiều may mắn trên đường đi của mình và có 1 cuộc sống vui vẻ hạnh phúc nhé 🥰',
+    desc: 'Và giờ thì mèo xin tạm biệt Đon nhé. Mãi yêuuuu ',
+    btnYes: 'Chạm vào tui đi ;)',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Mèo đi rồi nhưng vẫn còn 1 người, Đon có muốn người đó thay mèo bên cạnh Đon nữa không?',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Có, Không',
+    mess: 'okee cảm ơn Đon cho mèo biết nhé để mèo nói lại với nó cho meow~~~',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
