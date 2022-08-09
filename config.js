b3ef4494cfcd0e4b62c44024713d655d9c82a250
Tuikhongbttentuima <110917834+Tuikhongbttentuima@users.noreply.github.com>
2022-08-09 21:58:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "helo",
+    introTitle: 'khoong có',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
