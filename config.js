a64af4303f7cf5b3789f7640be62771b5bd48099
bro-emond-8 <82456637+bro-emond-8@users.noreply.github.com>
2022-10-28 19:56:06 +0800

    Create config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Bebe Khang",
+    introTitle: 'Hi, Khang! Pakisagot!',
+    introDesc: `Wala lang, gusto ko lang pasagutan sa 'yo hahah, okay lang ba?`,
+    btnIntro: 'Okay  sige, hihih',
+    title: 'Mahal mo ba ako baby khang? heheheh 🥰',
+    desc: 'Sagutin mo please huhuhuhuhuhuh',
+    btnYes: 'Oo naman, syempre ❤️',
+    btnNo: 'Hindi eii👍😢',
+    question: 'Marami namang iba, bakit mo ko lab?',
+    btnReply: 'Send to Emond',
+    reply: 'Kasi syempre, mahal kita',
+    mess: 'Alam ko naman yun heheh, iloveyouuuuu❤️',
+    messDesc: 'So, bati na tayo???',
+    btnAccept: 'Sige heheh',
+    messLink: 'https://www.facebook.com/emondpasion' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
