0154232e5ff0aa8109619b64e0c89bf4c90ad3c2
Nguyễn Thành Tài_60136781 <62944482+thanhtai290220@users.noreply.github.com>
2022-11-05 00:59:01 +0700

    Update config.js
    Q

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Tên người bạn',
+    introDesc: `❤️❤️❤️`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    title',
+    desc:  ',
+    btnYes: '❤️',
+    btnNo: '$',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
