c9581675e5a8e98666e1f2726ce104f75b2ee85b
tiendat25 <117836939+tiendat25@users.noreply.github.com>
2022-11-09 21:20:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "<3",
+    introTitle: 'Kiều Anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
