46d90248a37b87ff985e2cfe00f11dac4c96a27f
tiendat25 <117836939+tiendat25@users.noreply.github.com>
2022-11-09 21:25:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,11 +10,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao e lại yêu a <3',
+    btnReply: 'Gửi cho Kiều Anh <3',
+    reply: 'Tại vì yêu anh Đạt 11a1 vcl=))',
+    mess: 'A biết mà 🥰. Yêu e nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/caotiendat.14
 }
