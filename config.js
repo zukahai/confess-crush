4eb7a12d7557445f49b081b4c691ba87788e9dc8
capien <138313963+capien@users.noreply.github.com>
2023-07-01 22:55:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Xin Lũi Bé",
+    introTitle: 'Thanh Trúc siu sinh đẹp',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'Bé giận anh lúm đúng hông huhu',
+    desc: 'Cho anh chin lũi cục cưng nghe mai mún gì cũm chìu hihi giờ cho a địn nch đc hem  ',
+    btnYes: 'Dạ ocii anhhh iuu',
+    btnNo: 'Có cc chứ cho',
+    question: 'Tại sao bé iuuu anhh thế nhờ',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Tại anh đẹp zai khoai to vui tính và chúa tể vô duyên',
+    mess: 'ANh bít anh đẹp zai mà kaka anhh cũm iuu emm',
+    messDesc: 'Mai rước em đón em đi ăn ani siuu to đc hem',
+    btnAccept: 'Dạ ociii lunnn a zai khoai to ',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
