595974a7e6ad5fdd47d1a31ba65862bb687c6b61
nambmt97s <49761431+nambmt97s@users.noreply.github.com>
2023-11-27 18:49:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Xìn chào",
+    introTitle: 'Nguyễn Thị Kim Yến',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
